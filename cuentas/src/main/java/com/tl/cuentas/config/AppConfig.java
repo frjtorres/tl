87633f548367e7412a8af34bf12413c26a803bc9
frjtorres <franciscoj.torresm@hotmail.com>
2019-11-04 16:20:25 -0500

    ajuste de componentes para crear una conexion tcp hacia h2

--- cuentas/src/main/java/com/tl/cuentas/config/AppConfig.java
+++ cuentas/src/main/java/com/tl/cuentas/config/AppConfig.java
@@ -1,19 +1,26 @@
 package com.tl.cuentas.config;
 
+import org.h2.tools.Server;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AppConfig {
+
+    @Value("${spring.h2.server.port}")
+    private String h2Port;
 
     @Bean
     public AuditorAware auditorProvider() {
@@ -40,5 +47,11 @@ public class AppConfig {
         localValidator.setValidationMessageSource(messageSource());
 
         return localValidator;
+    }
+
+    @Profile("dev")
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server createH2TcpServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", h2Port);
     }
 }
